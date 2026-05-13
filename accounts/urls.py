70e0c3ae3d7952c4cfd5894548ebfbf9f4013dbb
shanr <shanrathnayak15@gmail.com>
2026-05-13 23:30:49 +0100

    mod: fixed the error in the register

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import LoginView, register_user
 
+
+app_name = "accounts"
+
 urlpatterns = [
     path("", LoginView.as_view(), name="login"),
     path("register/", register_user, name="register"),
