cef7435c2cda117fbe37572f4a0ce362e47ccf80
shanr <shanrathnayak15@gmail.com>
2026-05-13 22:59:43 +0100

    Add: Login HTML and CSS

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    role_choice = [
+        ("academic","Academic"),
+        ("cms_it","CMS IT")
+    ]
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=role_choice,
+        default="academic"
+    )
+
+    is_approved = models.BooleanField(default=False)
+    department = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from .models import UserProfile
 
 # Create your views here.
+
+def login_view(request):
+    return render(request, "accounts/login.html")
